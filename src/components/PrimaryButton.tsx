e3f85bb1419e04c478bd07fbdb80d0c21520902f
Zeinsptra <xeubrant@gmail.com>
2026-09-22 11:12:43 +0700

    Create PrimaryButton.tsx

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,27 @@
+📁 CWSMS-Project/
+├── 📄 package.json         
+├── 📄 index.js            
+├── 📄 App.tsx              
+├── 📄 .gitignore           
+└── 📁 src/                 
+    ├── 📁 assets/          
+    │
+    ├── 📁 components/      
+    │   ├── PrimaryButton.tsx
+    │   └── AppTextField.tsx
+    │
+    ├── 📁 screens/         
+    │   ├── ParentDashboardScreen.tsx
+    │   └── ChildShieldScreen.tsx
+    │
+    ├── 📁 navigation/       
+    │   └── AppNavigator.tsx
+    │
+    ├── 📁 services/        
+    │   └── usageStatsService.ts
+    │
+    ├── 📁 types/           
+    │   └── userType.ts
+    │
+    └── 📁 utils/            
+        └── timeFormatter.ts
