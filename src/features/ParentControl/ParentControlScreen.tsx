7645120eae55516b0b50c6a81ccedff26610d9ec
Zeinsptra <xeubrant@gmail.com>
2026-09-26 14:32:07 +0700

    Create ParentControlScreen.tsx

---
+++ src/features/ParentControl/ParentControlScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export const ParentControlScreen = () => {
+  return (
+
+      Parent Control - Screen Time
+
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' }
+});
